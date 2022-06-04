c6cde9eb8c665000abf13a4de0287e248382cb2e
Dani <daniel.roesch91@gmail.com>
2022-06-04 12:11:50 +0200

    CSS styling changes.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { ButtonBox } from "./components/ButtonBox.js";
 import { CalculatorButton } from "./components/CalculatorButton.js";
 import React, { useState } from 'react';
 
-// TODO: Implement additional advanced math features: sin, cos, arc, natural log, pi, etc. 
 
 // Array representing all values for the buttons.
 const buttonValues = [
